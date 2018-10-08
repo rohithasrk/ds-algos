9e375b75c7e2f418ce6ab0151aeea3b38a4999a4
Rohith Asrk <rohith.asrk@gmail.com>
2018-10-09 00:25:25 +0530

    Modify dungeon princess problem

--- algo/dp/dungeon_princess.cpp
+++ algo/dp/dungeon_princess.cpp
@@ -4,22 +4,13 @@ using namespace std;
 #define C 3
 
 int minPoints(int arr[R][C]){
-    for(int i=0; i<R; i++){
-        for(int j=0; j<C; j++){
-            arr[i][j] = -1*arr[i][j];
-        }
-    }
-    int dp[R][C];
-    dp[R-1][C-1] = max(1, arr[R-1][C-1]+1);
-    for(int j=C-2; j>=0; j--){
-        dp[R-1][j] = max(1, arr[R-1][j] + dp[R-1][j+1]);
-    }
-    for(int i=R-2; i>=0; i--){
-        dp[i][C-1] = max(1, arr[i][C-1] + dp[i+1][C-1]);
-    }
-    for(int i=R-2; i>=0; i--){
-        for(int j=C-2; j>=0; j--){
-            dp[i][j] = max(1, arr[i][j] + min(dp[i+1][j], dp[i][j+1]));
+    vector<vector<int>> dp(R+1, vector<int>(C+1, INT_MAX));
+    dp[R][C-1] = 1;
+    dp[R-1][C] = 1;
+    for(int i=R-1; i>=0; i--){
+        for(int j=C-1; j>=0; j--){
+            int need = min(dp[i+1][j], dp[i][j+1]) - arr[i][j];
+            dp[i][j] = need <=0 ? 1: need;
         }
     }
     return dp[0][0];
